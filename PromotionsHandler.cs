e0f3d077ba33912b66c8f3620f348f0325f468f3
AkshataChougule <89480158+AkshataChougule@users.noreply.github.com>
2021-08-29 16:41:15 +0530

    Add files via upload

---
+++ PromotionsHandler.cs
@@ -0,0 +1,16 @@
+﻿using PromotionsTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromotionsTask.Handlers
+{
+    public class PromotionsHandler : IPromotionsHandler
+    {
+        public Task<CartResponse> FetchCartValue(CartQuery cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
